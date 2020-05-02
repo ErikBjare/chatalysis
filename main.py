04b5609e5541c98ba0df5650895e383b8d54eb88
Erik Bjäreholt <erik@bjareho.lt>
2020-05-02 12:07:32 +0200

    style: formatted with black

--- main.py
+++ main.py
@@ -25,14 +25,16 @@ class Message:
     data: dict = field(default_factory=dict)
 
 
-cache_location = './.message_cache'
+cache_location = "./.message_cache"
 memory = Memory(cache_location, verbose=0)
 
 me = "Erik Bjäreholt"
 
 # Idk how this works, but it does
 # https://stackoverflow.com/a/26740753/965332
-re_emoji = re.compile(u'[\U00002600-\U000027BF]|[\U0001f300-\U0001f64F]|[\U0001f680-\U0001f6FF]')
+re_emoji = re.compile(
+    "[\U00002600-\U000027BF]|[\U0001f300-\U0001f64F]|[\U0001f680-\U0001f6FF]"
+)
 
 
 def main() -> None:
@@ -46,7 +48,7 @@ def main() -> None:
 
 
 def _most_reacted_msgs(msgs):
-    msgs = sorted(msgs, key=lambda m: -len(m.data['reacts']))
+    msgs = sorted(msgs, key=lambda m: -len(m.data["reacts"]))
     for m in msgs[:30]:
         _print_msg(m)
 
@@ -59,31 +61,42 @@ def _yearly_messaging_stats(msgs, name):
         year_msgs = [m for m in my_msgs if m.date.year == year]
         if not year_msgs:
             continue
-        rows.append((
-            year,
-            len(year_msgs),
-            sum(len(m.content.split(" ")) for m in year_msgs),  # words
-            sum(len(m.content) for m in year_msgs)  # chars
-        ))
-    print(tabulate(rows, headers=['year', '# msgs', 'words', 'chars']))
+        rows.append(
+            (
+                year,
+                len(year_msgs),
+                sum(len(m.content.split(" ")) for m in year_msgs),  # words
+                sum(len(m.content) for m in year_msgs),  # chars
+            )
+        )
+    print(tabulate(rows, headers=["year", "# msgs", "words", "chars"]))
 
 
 def top_writers(msgs):
     writerstats = defaultdict(lambda: Counter())
     for msg in msgs:
-        if msg.data.get('groupchat', False):
+        if msg.data.get("groupchat", False):
             continue
         s = writerstats[msg.from_name]
-        if 'days' not in s:
-            s['days'] = set()
-        s['days'] = s.get('days', set()) | {msg.date.date()}
-        s['msgs'] += 1
-        s['words'] += len(msg.content.split(" "))
-    writerstats = dict(sorted(writerstats.items(), key=lambda kv: kv[1]['msgs'], reverse=True))
+        if "days" not in s:
+            s["days"] = set()
+        s["days"] = s.get("days", set()) | {msg.date.date()}
+        s["msgs"] += 1
+        s["words"] += len(msg.content.split(" "))
+    writerstats = dict(
+        sorted(writerstats.items(), key=lambda kv: kv[1]["msgs"], reverse=True)
+    )
 
     wrapper = textwrap.TextWrapper(max_lines=1, width=30, placeholder="...")
-    print(tabulate([(wrapper.fill(k), v['msgs'], len(v['days']), v['words']) for k, v in writerstats.items()],
-                   headers=['name', 'msgs', 'days', 'words']))
+    print(
+        tabulate(
+            [
+                (wrapper.fill(k), v["msgs"], len(v["days"]), v["words"])
+                for k, v in writerstats.items()
+            ],
+            headers=["name", "msgs", "days", "words"],
+        )
+    )
 
 
 def _calculate_streak(days) -> int:
@@ -108,7 +121,10 @@ def _count_emoji(txt: str) -> Dict[str, int]:
 
 
 def _format_emojicount(emojicount: Dict[str, int]):
-    return ", ".join(f"{n}x {emoji}" for n, emoji in reversed(sorted((v, k) for k, v in emojicount.items())))
+    return ", ".join(
+        f"{n}x {emoji}"
+        for n, emoji in reversed(sorted((v, k) for k, v in emojicount.items()))
+    )
 
 
 def test_count_emoji() -> None:
@@ -136,6 +152,7 @@ def _convo_participants_key_undir(m: Message) -> str:
 def _calendar(msgs: List[Message]) -> Dict[date, List[Message]]:
     def datekey(m: Message):
         return m.date.date()
+
     grouped = groupby(sorted(msgs, key=datekey), key=datekey)
     msgs_per_date: Dict[date, List[Message]] = defaultdict(list)
     msgs_per_date.update({k: list(v) for k, v in grouped})
@@ -143,22 +160,29 @@ def _calendar(msgs: List[Message]) -> Dict[date, List[Message]]:
 
 
 def _people_stats(msgs: List[Message]) -> None:
-    grouped = groupby(sorted(msgs, key=_convo_participants_key_undir), key=_convo_participants_key_undir)
+    grouped = groupby(
+        sorted(msgs, key=_convo_participants_key_undir),
+        key=_convo_participants_key_undir,
+    )
     rows = []
     for k, _v in grouped:
         v = list(_v)
         days = {m.date.date() for m in v}
-        rows.append((
-            k[:40],
-            len(v),
-            len(days),
-            _calculate_streak(days),
-            _format_emojicount(dict(_most_used_emoji(m.content for m in v).most_common()[:5]))
-        ))
-    print(tabulate(rows, headers=['k', 'days', 'max streak', 'most used emoji']))
+        rows.append(
+            (
+                k[:40],
+                len(v),
+                len(days),
+                _calculate_streak(days),
+                _format_emojicount(
+                    dict(_most_used_emoji(m.content for m in v).most_common()[:5])
+                ),
+            )
+        )
+    print(tabulate(rows, headers=["k", "days", "max streak", "most used emoji"]))
 
 
-def _get_all_chat_files(glob='*'):
+def _get_all_chat_files(glob="*"):
     msgdir = Path("data/private/messages")
     return sorted(list(msgdir.glob(f"{glob}/message.html")))
 
@@ -176,8 +200,12 @@ def _is_groupchat(soup):
     return soup.select("div[role='main'] > div")[0].text.startswith("Participants:")
 
 
-def _parse_all_messages(glob: str = '*') -> List[Message]:
-    messages = [msg for filename in _get_all_chat_files(glob) for msg in _parse_messages(filename)]
+def _parse_all_messages(glob: str = "*") -> List[Message]:
+    messages = [
+        msg
+        for filename in _get_all_chat_files(glob)
+        for msg in _parse_messages(filename)
+    ]
     log.info(f"Parsed {len(messages)} messages")
     return messages
 
@@ -198,13 +226,23 @@ def _parse_messages(filename) -> List[Message]:
                 sub = msg.find_all("div")
                 pfrom = sub[0].text
                 pto = me if not is_groupchat and pfrom != me else other
-                text = sub[1].select('div')[2].text
-                reacts = [(el.text[0], el.text[1:]) for el in sub[1].select("ul._tqp > li")]
+                text = sub[1].select("div")[2].text
+                reacts = [
+                    (el.text[0], el.text[1:]) for el in sub[1].select("ul._tqp > li")
+                ]
                 datestr = sub[-1].text
                 date = datetime.strptime(datestr, "%b %d, %Y %I:%M%p")
                 if "You are now connected" in text:
                     continue
-                messages.append(Message(pfrom, pto, date, text, data={'groupchat': is_groupchat, 'reacts': reacts}))
+                messages.append(
+                    Message(
+                        pfrom,
+                        pto,
+                        date,
+                        text,
+                        data={"groupchat": is_groupchat, "reacts": reacts},
+                    )
+                )
             except IndexError as e:
                 log.warning(f"Unable to parse: {e} (in {filename} for msg {msg})")
             except ValueError as e:
@@ -213,8 +251,12 @@ def _parse_messages(filename) -> List[Message]:
 
 
 def _print_msg(msg: Message) -> None:
-    emojicount_str = _format_emojicount(_count_emoji(''.join(t[0] for t in msg.data['reacts'])))
-    print(f"{msg.date.isoformat()[:10]} | {msg.from_name} -> {msg.to_name}: {msg.content}  ({emojicount_str})")
+    emojicount_str = _format_emojicount(
+        _count_emoji("".join(t[0] for t in msg.data["reacts"]))
+    )
+    print(
+        f"{msg.date.isoformat()[:10]} | {msg.from_name} -> {msg.to_name}: {msg.content}  ({emojicount_str})"
+    )
 
 
 if __name__ == "__main__":
